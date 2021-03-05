fba1270282629614817e20137371aed036ff13af
Jayson Presto <jaysonpresto.iprog21@gmail.com>
2021-03-05 21:29:16 +0800

    Build Pokedex Filter

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,20 @@
 class PokemonsController < ApplicationController
   def index
-    pokedex = PokeApi.get(pokedex: params[:pokedex] || 1)
-    @pokemon_entries = pokedex.pokemon_entries.size
+    @pokedex_id = params[:pokedex_id] || 1
+    pokedex = PokeApi.get(pokedex: @pokedex_id)
+    @pokemon_entries = pokedex.pokemon_entries
+    # render json: @pokemon_entries and return
   end
+
+  def pokedex_lists
+    lists = []
+    for i in 1..9
+      lists << ["Pokedex #{i}", i]
+    end
+    lists
+  end
+  helper_method :pokedex_lists
+
+  protected
+
 end
